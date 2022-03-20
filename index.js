36ad2addd8f6b801eeb53bc183c8a90b638d8122
Lubin Cardenas <lubin.cardenas15@gmail.com>
2022-03-19 23:19:38 -0400

    g

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
+import {USERS_NEW} from './users.js'
+    
+    dotenv.config();
     const port = 3000;
     const expressApp = express();
-    expressApp.listen(port, () => console.log(`servidor levantado en el puerto ${port}`));
+    
+    
+
     expressApp.use(express.json()); 
     expressApp.use(express.text()); 
 
-    expressApp.post("/cuenta", (req,res)=> {
-    console.log(req.query);
-    res.send();  
-    }); 
+    //obtener los detalles de una cuenta apartir del guid
+    expressApp.get('/account/:guid', (req,res)=>{
+        const { guid } = req.params;
+        const user = USERS_NEW.find(user => user.guid === guid);
+        if(!user) return res.status(404).send();
+        res.send(user);
+    });
+    //crear una nueva cuenta a partir de guid y name
+    expressApp.post('/account', (req,res)=>{
+        const { guid, name } = req.body;
 
-    expressApp.put("/producto", (req,res)=> {
-    console.log(req.body);
-    }); 
-  
+        if(!name || !guid) return res.status(400).send();
+
+        const user = USERS_NEW.find(user => user.guid === guid);
+        if(user) return res.status(409).send();
+
+        USERS_NEW.push({
+            guid, name
+        });
+        res.send();
+    })
+    //actualizar el nombre de una cuenta
+    expressApp.patch('/account/:guid', (req,res)=>{
+        const { guid } = req.params;
+        const { name } = req.body;
+
+        if(!name) res.status(400).send();
+
+        const user = USERS_NEW.find(user => user.guid === guid)
+
+        if (!user) res.status(404).send();
+
+        user.name = name;
+
+        return res.send();
+    })
+    //eliminar una cuenta
+    expressApp.delete('/account/:guid', (req,res)=>{
+        const { guid } = req.params;
+        const userIndex = USERS_NEW.findIndex(user => user.guid === guid);
+        if(userIndex === -1) return res.status(404).send();
+
+        USERS_NEW.splice(userIndex, 1);
+        res.send();
+    })
+
+    expressApp.listen(port, () => console.log(`servidor levantado en el puerto ${port}`));
+
+
